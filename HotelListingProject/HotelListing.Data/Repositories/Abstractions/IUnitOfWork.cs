7e3421469471db007e1b0a4f69b22a10800ab081
Ikechukwu <ikechukwu.njoku@decagon.dev>
2021-10-16 16:56:22 +0100

    Added the repository components

---
+++ HotelListingProject/HotelListing.Data/Repositories/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using HotelListingProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data.Repositories.Abstractions
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+
+        Task Save();
+    }
+}
---
+++ HotelListingProject/HotelListing.Data/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,89 @@
+﻿using HotelListing.Data.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data.Repositories.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly Context _context;
+        private readonly DbSet<T> _db;
+        public GenericRepository(Context context)
+        {
+            _context = context;
+            _db = _context.Set<T>();
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await _db.FindAsync(id);
+            _db.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _db.RemoveRange(entities);
+        }
+
+        public async Task<T> Get(System.Linq.Expressions.Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+            //check if there are additional details and include them in query
+            if(includes != null) 
+            {
+                foreach(var item in includes) 
+                {
+                    query = query.Include(item);
+                }
+            }
+
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+
+            //check if there's eny expression
+            if (expression != null) 
+            {
+                query = query.Where(expression);
+            }
+            //check if there are additional details and include them in query
+            if (includes != null)
+            {
+                foreach (var item in includes)
+                {
+                    query = query.Include(item);
+                }
+            }
+
+            if(orderBy != null) 
+            {
+                query = orderBy(query);
+            }
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _db.AddAsync(entity);
+        }
+
+        public async Task InsertRange(IEnumerable<T> entities)
+        {
+            await _db.AddRangeAsync(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _db.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
