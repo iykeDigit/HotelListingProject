709280e7dd88f0750402f2047dc0263e4c4ca825
Ikechukwu <ikechukwu.njoku@decagon.dev>
2021-10-16 15:37:10 +0100

    Created the additional projects

---
+++ HotelListingProject/HotelListing.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HotelListing.Core
+{
+    public class Class1
+    {
+    }
+}
--- HotelListingProject/HotelListingProject/Startup.cs
+++ HotelListingProject/HotelListingProject/Startup.cs
@@ -1,7 +1,9 @@
+using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,8 +29,12 @@ namespace HotelListingProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+
+            );
             services.AddSingleton(Log.Logger);
-            services.AddControllers();
+            
             services.AddCors(o =>
             {
                 o.AddPolicy("AllowAll", builder =>
@@ -40,6 +46,7 @@ namespace HotelListingProject
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListingProject", Version = "v1" });
             });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
